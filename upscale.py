6de4f473ec71b3432edb8b76619855c58088e819
AndreiaAgost <83586497+AndreiaAgost@users.noreply.github.com>
2021-05-21 12:40:10 +0100

    adicionado a função de upscale com 3 canais

--- upscale.py
+++ upscale.py
@@ -27,6 +27,23 @@ def upscale_image(model, img):
     )
     return out_img
 
+def upscale_image_3_channels(model, img):
+    """Predict the result based on input image and restore the image as RGB."""
+    y = img_to_array(img)
+    y = y.astype("float32") / 255.0
+
+    input = np.expand_dims(y, axis=0)
+    out = model.predict(input)
+
+    out_img_y = out[0]
+    out_img_y *= 255.0
+
+    # Restore the image in RGB color space.
+    out_img_y = out_img_y.clip(0, 255)
+    out_img_y = out_img_y.reshape((np.shape(out_img_y)[0], np.shape(out_img_y)[1], 3))
+    out_img_y = PIL.Image.fromarray(np.uint8(out_img_y), mode="RGB")
+    return out_img_y
+
 
 UPSCALE_MODEL = load_model('model.h5')
 
